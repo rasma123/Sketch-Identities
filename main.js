8e48dffe18ac8423f46d531bf94655ec3fceecc3
rasma123 <82595793+rasma123@users.noreply.github.com>
2022-11-12 19:39:13 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,33 @@
+function setup() {
+    canvas = createCanvas(280 , 280);
+    canvas.position(560 , 250);
+    background("white");
+    canvas.mouseReleased(classifyCanvas);
+    synth = window.speechSynthesis;
+}
+function clearCanvas() {
+    background("white");
+}
+function preload() {
+    classifier = ml5.imageClassifier('DoodleNet');
+}
+function draw() {
+ strokeWeight(13);
+ stroke(0);
+ if(mouseIsPressed) {
+    line(pmouseX,pmouseY,mouseX,mouseY);
+ }   
+}
+function classifyCanvas() {
+classifier.classify(canvas,gotResult);}
+function gotResult(error,results) {
+    if (error) {
+        console.error(error);
+    }
+    console.log(results);
+    document.getElementById('label').innerHTML = 'label: ' + results[0].label;
+
+    document.getElementById('confidence').innerHTML = 'confidence: '+ Math.round(results[0].confidence * 100) + '%';
+    UtterThis = new SpeechSynthesisUtterance(results[0].label);
+    synth.speak(UtterThis);
+}
